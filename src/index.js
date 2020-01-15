05c81961fc1b63d0353079f6b4af05cbef42a612
TNanukem <tiago.nanu@gmail.com>
2020-01-15 18:55:32 -0300

    Form added and changed the structure of views files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 var app = express();
 
 app.set('view engine', 'pug');
 app.set('views','./views');
+
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array());
 
 app.use(express.static('public'));
 
@@ -10,8 +24,13 @@ app.get('/', function(req, res){
    res.render("index");
 });
 
-app.get('/first_template', function(req, res){
-   res.render('index');
-});
+app.get('/login', function(req, res) {
+  res.render("login")
+})
+
+app.post('/login', function(req, res){
+  console.log(req.body);
+  res.send("Logged");
+})
 
 app.listen(3000);
