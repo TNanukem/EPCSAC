5e857196291dec8c78d100391f63326ae414e20a
TNanukem <tiago.nanu@gmail.com>
2020-01-18 15:54:27 -0300

    Database configuration and gitignore added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var { pool } = require('./config')
+const cors = require('cors')
+
 var upload = multer();
 var app = express();
 
 app.set('view engine', 'pug');
 app.set('views','./views');
-
 
 // for parsing application/json
 app.use(bodyParser.json());
@@ -22,6 +24,10 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res){
    res.render("index");
+   // pool.query(array, (err, res) => {
+      // console.log(err, res)
+      // pool.end()
+   // })
 });
 
 app.get('/login', function(req, res) {
