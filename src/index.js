fb8d8c75d22e05823ec10d683cdd4800b18c3986
TNanukem <tiago.nanu@gmail.com>
2020-01-31 15:59:15 -0300

    Added all configuration parameters

--- src/index.js
+++ src/index.js
@@ -335,9 +335,37 @@ app.post('/config', function(req, res){
     console.log("max:",cloudlet_length_max);
     console.log("min:",cloudlet_length_min);
 
+    var checkbox_iterations = req.body.checkbox_iterations;
+    var iterations_exact = req.body.iterations_exact;
+
+    console.log("\nIterations");
+    console.log("exact:",iterations_exact);
+
+    var checkbox_datacenters = req.body.checkbox_datacenters;
+
+    if(checkbox_datacenters != null){
+      var datacenters_max = req.body.datacenters_max;
+      var datacenters_min = req.body.datacenters_min;
+      var datacenters_exact = null;
+      var datacenters_flag = true;
+    }
+
+    else {
+      var datacenters_max = null;
+      var datacenters_min = null;
+      var datacenters_exact = req.body.datacenters_exact;
+      var datacenters_flag = false;
+    }
+
+    console.log("\nDatacenters");
+    console.log("checkbox:",checkbox_datacenters);
+    console.log("exact:",datacenters_exact);
+    console.log("max:",datacenters_max);
+    console.log("min:",datacenters_min);
+
     // Updates the parameters table
     pool.query(
-      'INSERT INTO parameters (iterations ,hosts_exact,hosts_pes_exact,hosts_ram_exact,hosts_bw_exact,hosts_hd_exact,vms_exact,vms_pes_exact,vms_ram_exact,vms_bw_exact,vms_hd_exact,cloudlets_exact,cloudlets_pes_exact,cloudlets_length_exact, datacenters_max,datacenters_min,datacenters_flag,hosts_max,hosts_min,hosts_flag,hosts_pes_max,hosts_pes_min,hosts_pes_flag,hosts_ram_max,hosts_ram_min,hosts_ram_flag,hosts_bw_max,hosts_bw_min,hosts_bw_flag,hosts_hd_max,hosts_hd_min,hosts_hd_flag,vms_max,vms_min,vms_flag,vms_pes_max,vms_pes_min,vms_pes_flag,vms_ram_max,vms_ram_min,vms_ram_flag,vms_bw_max,vms_bw_min,vms_bw_flag,vms_hd_max,vms_hd_min,vms_hd_flag,cloudlets_max,cloudlets_min,cloudlets_flag,cloudlets_pes_max,cloudlets_pes_min,cloudlets_pes_flag,cloudlets_length_min,cloudlets_length_flag,cloudlets_length_max) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49, $50, $51, $52, $53, $54, $55, $56)', [5, host_exact, host_pes_exact, host_ram_exact, host_bw_exact, host_hd_exact, vm_exact, vm_pes_exact, vm_ram_exact, vm_bw_exact, vm_hd_exact, cloudlet_exact, cloudlet_pes_exact, cloudlet_length_exact, 1, 1, false, host_max, host_min, host_flag, host_pes_max, host_pes_min, host_pes_flag, host_ram_max, host_ram_min, host_ram_flag, host_bw_max, host_bw_min, host_bw_flag, host_hd_max, host_hd_min, host_hd_flag,vm_max, vm_min, vm_flag, vm_pes_max, vm_pes_min, vm_pes_flag, vm_ram_max, vm_ram_min, vm_ram_flag, vm_bw_max, vm_bw_min, vm_bw_flag, vm_hd_max, vm_hd_min, vm_hd_flag, cloudlet_max, cloudlet_min, cloudlet_flag, cloudlet_pes_max, cloudlet_pes_min, cloudlet_pes_flag, cloudlet_length_min,cloudlet_length_flag,cloudlet_length_max], error => {
+      'INSERT INTO parameters (iterations ,hosts_exact,hosts_pes_exact,hosts_ram_exact,hosts_bw_exact,hosts_hd_exact,vms_exact,vms_pes_exact,vms_ram_exact,vms_bw_exact,vms_hd_exact,cloudlets_exact,cloudlets_pes_exact,cloudlets_length_exact, datacenters_max,datacenters_min,datacenters_flag,hosts_max,hosts_min,hosts_flag,hosts_pes_max,hosts_pes_min,hosts_pes_flag,hosts_ram_max,hosts_ram_min,hosts_ram_flag,hosts_bw_max,hosts_bw_min,hosts_bw_flag,hosts_hd_max,hosts_hd_min,hosts_hd_flag,vms_max,vms_min,vms_flag,vms_pes_max,vms_pes_min,vms_pes_flag,vms_ram_max,vms_ram_min,vms_ram_flag,vms_bw_max,vms_bw_min,vms_bw_flag,vms_hd_max,vms_hd_min,vms_hd_flag,cloudlets_max,cloudlets_min,cloudlets_flag,cloudlets_pes_max,cloudlets_pes_min,cloudlets_pes_flag,cloudlets_length_min,cloudlets_length_flag,cloudlets_length_max) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49, $50, $51, $52, $53, $54, $55, $56)', [iterations_exact, host_exact, host_pes_exact, host_ram_exact, host_bw_exact, host_hd_exact, vm_exact, vm_pes_exact, vm_ram_exact, vm_bw_exact, vm_hd_exact, cloudlet_exact, cloudlet_pes_exact, cloudlet_length_exact, datacenters_max, datacenters_min, datacenters_flag, host_max, host_min, host_flag, host_pes_max, host_pes_min, host_pes_flag, host_ram_max, host_ram_min, host_ram_flag, host_bw_max, host_bw_min, host_bw_flag, host_hd_max, host_hd_min, host_hd_flag,vm_max, vm_min, vm_flag, vm_pes_max, vm_pes_min, vm_pes_flag, vm_ram_max, vm_ram_min, vm_ram_flag, vm_bw_max, vm_bw_min, vm_bw_flag, vm_hd_max, vm_hd_min, vm_hd_flag, cloudlet_max, cloudlet_min, cloudlet_flag, cloudlet_pes_max, cloudlet_pes_min, cloudlet_pes_flag, cloudlet_length_min,cloudlet_length_flag,cloudlet_length_max], error => {
 
       if (error) {
         throw error
