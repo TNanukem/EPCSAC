50924e571e64d756b6defe8a68f73d806419cd86
Tiago Toledo Junior <33360760+TNanukem@users.noreply.github.com>
2020-06-07 15:38:49 -0300

    Fixed link on e-mail

--- src/helpers/simulation.js
+++ src/helpers/simulation.js
@@ -91,7 +91,7 @@ const Simulation = {
     });
 
     host = "http://epcsac.lasdpc.icmc.usp.br/"
-    link = "http://epcsac.lasdpc.icmc.usp.br/downloadSimulation?token="+String(token)+"&user_id="+String(req.session.user_id);
+    link = "http://epcsac.lasdpc.icmc.usp.br/dashboard?token="+String(token)+"&user_id="+String(req.session.user_id);;
 
     var mailOptions = {
         to : req.session.email,
